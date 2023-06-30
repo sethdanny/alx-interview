2579f2bf58f9c1f694983288720c7124bb359536
sethdanny <naddulidaniel1994@gmail.com>
2023-07-01 01:22:37 +0300

    challenge solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-""" pascals triangle to solve coefficients powers
-"""
+"""Pascal Triangle Challenge"""
 
 
 def pascal_triangle(n):
-     """returns a list of lists of numbers
+    """returns a list of lists of numbers
     representing the pacals triangle"""
-     if n <= 0:
+    if n <= 0:
         return []
-    
-     triangle = [[1]]
-    
-     for i in range(1, n):
-        row = [1]
-        prev_row = triangle[i-1]
-        
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
-        
-        row.append(1)
-        triangle.append(row)
-    
-     return triangle
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
