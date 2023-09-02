3588eebda0e12b9b580bf72dea387ed8464c5686
sethdanny <naddulidaniel1994@gmail.com>
2023-09-02 11:18:33 +0300

    calculate perimeter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""find perimeter of an island  in a grid"""
+
+
+def island_perimeter(grid):
+    """find perimeter of an island  in a grid"""
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
